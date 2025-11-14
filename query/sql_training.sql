ed69a569220cd8c3090645d134187499448e4fbc
maarwanali <maarwan.a.ali@gmail.com>
2025-11-14 17:31:58 +0300

    some modifications

--- query/sql_training.sql
+++ query/sql_training.sql
@@ -3,13 +3,15 @@
 -- START
 
 SELECT 
-	F.category_id, C.name , count(F.film_id) as films_count
+	f.category_id, 
+	c.name, 
+	count(f.film_id) as films_count
 FROM 
-	film_category F 
+	film_category f
 INNER JOIN 
-	category C ON F.category_id = C.category_id
+	category c ON f.category_id = c.category_id
 GROUP BY 
-	C.name, F.category_id
+	c.name, f.category_id
 ORDER BY 
 	films_count DESC;
 -- END
@@ -17,49 +19,46 @@ ORDER BY
 -- Output the 10 actors whose movies rented the most, sorted in descending order.-----------------------
 
 -- START
-WITH film_rental_counts AS (
-	SELECT 
-		I.film_id, count(R.rental_id) as rental_count
-	FROM 
-		rental R 
-	INNER JOIN 
-		inventory I ON R.inventory_id = I.inventory_id
-	GROUP BY 
-		I.film_id 
-	)
+
 SELECT 
-	F.actor_id, A.last_name || ' ' || A.first_name as full_name, SUM(R.rental_count) as total_rent_per_actor
+	a.actor_id,
+	CONCAT(a.last_name, ' ', a.first_name),
+	COUNT(r.rental_id) as total_rent_per_actor
 FROM 
-	film_actor F
-INNER JOIN 
-	film_rental_counts R ON F.film_id = R.film_id
-INNER JOIN 
-	actor A ON F.actor_id = A.actor_id
+	rental r 
+JOIN 
+	inventory i ON r.inventory_id = i.inventory_id 
+JOIN 
+	film_actor fa on fa.film_id = i.film_id 
+JOIN 
+	actor a ON a.actor_id = fa.actor_id
 GROUP BY 
-	F.actor_id, A.last_name, A.first_name
+	a.actor_id, a.last_name, a.first_name
 ORDER BY 
-	total_rent_per_actor DESC 
-LIMIT 10;
+	total_rent_per_actor DESC
+LIMIT 10
+	
+
 -- END
 
 --  Output the category of movies on which the most money was spent. -------
 
 -- START
 SELECT
-    C.name,
-    SUM(P.amount) AS total_revenue
+    c.name,
+    SUM(p.amount) AS total_revenue
 FROM
-    payment P
+    payment p
 INNER JOIN
-    rental R ON P.rental_id = R.rental_id
+    rental r ON p.rental_id = r.rental_id
 INNER JOIN
-    inventory I ON R.inventory_id = I.inventory_id
+    inventory i ON r.inventory_id = i.inventory_id
 INNER JOIN
-    film_category FC ON I.film_id = FC.film_id
+    film_category fc ON i.film_id = fc.film_id
 INNER JOIN
-    category C ON FC.category_id = C.category_id
+    category c ON fc.category_id = c.category_id
 GROUP BY
-    C.name
+    c.name
 ORDER BY
     total_revenue DESC
 LIMIT 1;
@@ -69,13 +68,14 @@ LIMIT 1;
 
 -- START
 SELECT 
-	 F.film_id, F.title 
+	 f.film_id, 
+	 f.title 
 FROM 
-	inventory I 
-RIGHT JOIN 
-	film F ON I.film_id = F.film_id
+	film f
+LEFT JOIN 
+	inventory i ON i.film_id = f.film_id
 WHERE 
-	I.film_id is NULL
+	i.film_id is NULL
 ORDER BY title;
 -- END
 
@@ -83,48 +83,33 @@ ORDER BY title;
 Output the top 3 actors who have appeared the most in movies in the “Children” category.
 If several actors have the same number of movies, output all of them.
 */
-
+--  1 subquery 🫣
 -- START
-WITH children_category AS
-(
-SELECT film_id 
-FROM 
-	film_category
-WHERE 
-	category_id = (
-	SELECT
-		category_id
-	FROM 
-		category
-	WHERE
-		name = 'Children'
-	)
-),
-actor_counts AS (SELECT 
-	FA.actor_id, CONCAT(A.last_name,' ',A.first_name) as full_name, count(CC.film_id) as films_count
-FROM 
-	children_category CC
-JOIN 
-	film_actor FA ON CC.film_id = FA.film_id
-JOIN 
-	actor A ON A.actor_id = FA.actor_id
-GROUP BY 
-	FA.actor_id, A.last_name, A.first_name
-)
 SELECT 
-	full_name, films_count, rank_num
-FROM 
-	(
+	actor_id,
+	full_name,
+	rank_num
+	FROM (
 	SELECT 
-		*, DENSE_RANK() OVER ( ORDER BY films_count DESC) as rank_num
-	FROM
-		actor_counts
-	) AS ranked_actors
+		a.actor_id,
+		CONCAT(a.last_name, ' ', a.first_name) as full_name,
+		DENSE_RANK() OVER ( ORDER BY COUNT(fc.film_id) DESC) as rank_num
+	FROM 
+		film_category fc
+	JOIN 
+		category c ON fc.category_id = c.category_id
+	JOIN 
+		film_actor fa ON fa.film_id = fc.film_id
+	JOIN 
+		actor a ON a.actor_id = fa.actor_id
+	WHERE 
+		c.name = 'Children'
+	GROUP BY 
+		a.actor_id, a.last_name, a.first_name
+)
 WHERE 
 	rank_num <= 3
-ORDER BY 
-	rank_num, full_name;
-	
+
 -- END
 
 /*
@@ -135,21 +120,20 @@ ORDER BY
 -- START 
 
 SELECT 
-	A.city_id , 
-	CT.city,
-	SUM(CASE WHEN C.active = 1 THEN 1 ELSE 0 END) as active_count, 
-	SUM(CASE WHEN C.active = 0 THEN 1 ELSE 0 END) as inactive_count
+	a.city_id, 
+	ct.city,
+	SUM(CASE WHEN c.active = 1 THEN 1 ELSE 0 END) as active_count, 
+	SUM(CASE WHEN c.active = 0 THEN 1 ELSE 0 END) as inactive_count
 FROM 
-	customer C
+	customer c
 JOIN
-	address A ON C.address_id = A.address_id
+	address a ON c.address_id = a.address_id
 JOIN 
-	city CT on CT.city_id = A.city_id
+	city ct on ct.city_id = a.city_id
 GROUP BY 
-	A.city_id, CT.city
+	a.city_id, ct.city
 ORDER BY 
 	inactive_count DESC
-
 
 
 -- END
@@ -159,55 +143,58 @@ Output the category of movies that have the highest number of total rental hours
 		(customer.address_id in this city) and that start with the letter “a”. 
 Do the same for cities that have a “-” in them. Write everything in one query.
 */
-
--- START 
+-- START
 WITH category_rent_hours AS  ( 
 	SELECT
-		C.city, Ca.name as category_name,
-		SUM(EXTRACT(DAY FROM (R.return_date - rental_date)) * 24 
-		+ EXTRACT(HOUR FROM (R.return_date - rental_date)) 
-		+ EXTRACT(MINUTE FROM ( R.return_date - rental_date)) /60 )AS total_rent_hours
+		c.city, 
+		ca.name as category_name,
+		SUM(EXTRACT(DAY FROM (r.return_date - r.rental_date)) * 24 
+		+ EXTRACT(HOUR FROM (r.return_date - r.rental_date)) 
+		+ EXTRACT(MINUTE FROM ( r.return_date - r.rental_date)) /60 )AS total_rent_hours
 	FROM 
-		rental R
+		rental r
 	JOIN 
-		customer Cu ON R.customer_id = Cu.customer_id
+		customer cu ON r.customer_id = cu.customer_id
 	JOIN
-		address Ad ON Cu.address_id = Ad.address_id
+		address ad ON cu.address_id = ad.address_id
 	JOIN 
-		city C ON C.city_id = Ad.city_id
+		city C ON c.city_id = ad.city_id
 	JOIN 
-		inventory I ON R.inventory_id = I.inventory_id
+		inventory i ON r.inventory_id = i.inventory_id
 	JOIN 
-		film_category FC ON FC.film_id = I.film_id
+		film_category fc ON fc.film_id = i.film_id
 	JOIN  
-		category Ca ON Ca.category_id = FC.category_id
+		category ca ON ca.category_id = fc.category_id
 	GROUP BY 
-		Ca.name, C.city
+		ca.name, c.city
 ),
-cities_start_with_a AS (
+cities_starts_with_a AS (
 
 	SELECT 
-		'Cities Starting with A' as group_description, city, category_name, total_rent_hours,
+		'Cities Starting with a' as group_description, 
+		city, category_name, 
+		total_rent_hours,
 		ROW_NUMBER() OVER(ORDER BY total_rent_hours DESC) as rn
 	FROM 
 		category_rent_hours 
 	WHERE 
-		city LIKE 'a%' ),
+		city ILIKE 'a%' ),
 cities_with_dash AS
 (
 		SELECT 
-		'Cities have "-".' as group_description, city, category_name, total_rent_hours,
+		'Cities have "-".' as group_description, 
+		city, category_name, 
+		total_rent_hours,
 		ROW_NUMBER() OVER(ORDER BY total_rent_hours DESC) as rn
 	FROM 
 		category_rent_hours 
 	WHERE 
 		city LIKE '%-%' 
 )
-
 SELECT 
 	group_description, city, category_name, total_rent_hours
 FROM 
-	cities_start_with_a
+	cities_starts_with_a
 WHERE
 	rn = 1
 UNION ALL
@@ -217,5 +204,5 @@ FROM
 	cities_with_dash
 WHERE
 	rn = 1;
-	
--- END 
+
+-- END
